ced37f87f0591bf94b9cc3ee07ff7f7a39e92a6d
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-04-13 17:43:55 +0200

    update build for 16.03

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 erlc \
 	-o ebin \
-	-I include -I /opt/ejabberd-16.02/lib/ejabberd-16.02/include/ \
-	-I /opt/ejabberd-16.02/lib/p1_xmlrpc-1.15.1/include/ \
-        -I /opt/ejabberd-16.02/lib/fast_xml-1.1.3/include/ \
-	-pa /opt/ejabberd-16.02/lib/lager-3.0.2/ebin/ \
+	-I include -I /opt/ejabberd-16.03/lib/ejabberd-16.03/include/ \
+	-I /opt/ejabberd-16.03/lib/p1_xmlrpc-1.15.1/include/ \
+        -I /opt/ejabberd-16.03/lib/fast_xml-1.1.3/include/ \
+	-pa /opt/ejabberd-16.03/lib/lager-3.0.2/ebin/ \
     -DLAGER -DNO_EXT_LIB \
     src/*erl
