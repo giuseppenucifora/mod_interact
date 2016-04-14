f7ebcc6562b1a0cf5d158da83e4e3e2c67df1986
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-04-14 14:45:31 +0200

    Test

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -61,42 +61,8 @@ stop(Host) ->
   ok.
 
 send_notice(Packet, C2SState, From, To) ->
-  ?INFO_MSG("mod_offline_post called send_notice", []),
-  %Type = xml:get_tag_attr_s(list_to_binary("type"), Packet),
-  %Body = xml:get_path_s(Packet, [{elem, list_to_binary("body")}, cdata]),
-  %Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) ->
-  %  iolist_to_binary(S) end, list_to_binary("")),
-  %PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) ->
-  %  iolist_to_binary(S) end, list_to_binary("")),
-  %Format = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, body_format, fun(S) ->
-  %  iolist_to_binary(S) end, iolist_to_binary("")),
-  %OfflineMessageCount = get_queue_length(To#jid.luser, To#jid.lserver),
-  %?INFO_MSG("From ~s", [From]),
-  %?INFO_MSG("To ~s", [To]),
-  %?INFO_MSG("Packet ~s", [Packet]),
-  ok;
-
-%%if ((Type == <<"chat">>) or (Type == <<"groupchat">>)) and (Body /= <<"">>) ->
-%%    Post = case Format of
-%%               <<"post">> -> Sep = "&",
-%%                   ["to=", To#jid.luser, Sep,
-%%                       "from=", From#jid.luser, Sep,
-%%                       "body=", url_encode(binary_to_list(Body)), Sep,
-%%                       "access_token=", Token, Sep,
-%%                       "offline_message_count=", integer_to_list(OfflineMessageCount)];
-%%               _ -> Data = [{"to", To#jid.luser},
-%%                   {"from", From#jid.luser},
-%%                   {"body", Body},
-%%                   {"access_token", Token},
-%%                   {"offline_message_count", OfflineMessageCount}],
-%%                   mochijson2:encode({struct, Data})
-%%           end,
-%%    ?INFO_MSG("Sending post request to ~s with body \"~s\"", [PostUrl, Post]),
-%%    httpc:request(post, {binary_to_list(PostUrl), [], "application/x-www-form-urlencoded", list_to_binary(Post)},[],[]),
-%%    ok;
-%%    true -> ok
-%%end.
-
+  ?INFO_MSG("mod_offline_post called send_notice", [Packet]),
+  ok.
 
 %% Get number of offline messages for a user
 get_queue_length(LUser, LServer) ->
