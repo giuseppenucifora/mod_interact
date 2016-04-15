1b73b8ad87ec46b0c351e2fcf50bb377875da6f6
Giuseppe Nucifora <me@giuseppenucifora.com>
2016-04-15 15:09:51 +0200

    no message

--- src/mod_muc_offline_post.erl
+++ src/mod_muc_offline_post.erl
@@ -76,11 +76,8 @@ grab_notice(Packet = #xmlel{name = <<"message">>, attrs = Attrs}, From, To) ->
   end.
 
 
-send_notice(From, To, Packet = #xmlel{name = <<"message">>, attrs = Attrs, children = Children}) ->
+send_notice(From, To, Packet) ->
   ?INFO_MSG("Called send_notice ~p~n", [Packet]),
-  ?INFO_MSG("------------------------------------------------------", []),
-  ?INFO_MSG("------------------------------------------------------", []),
-  ?INFO_MSG("Children ~p~n", [Children]),
   Child = xml:get_tag_cdata(xml:get_subtag(Packet, <<"body">>)),
   ?INFO_MSG("------------------------------------------------------", []),
   ?INFO_MSG("------------------------------------------------------", []),
